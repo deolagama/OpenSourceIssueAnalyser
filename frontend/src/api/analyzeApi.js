507d5964dafe7bbca686752f4bd6f635a43db0ed
Deola <deolagamaa@gmail.com>
2026-03-08 19:48:59 +0000

    Integrated JWT

--- frontend/src/api/analyzeApi.js
+++ frontend/src/api/analyzeApi.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const API = "https://reimagined-dollop-7vw9p5p94p773x9v9-5000.app.github.dev";
+
 export const analyzeRepo = async (repo) => {
-  const res = await axios.post("http://localhost:5000/api/analyze", { repo });
+  const res = await axios.post(`${API}/api/analyze`, { repo });
   return res.data;
 };
